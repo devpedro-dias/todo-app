0b0e1dabc12b0d3b9ff5d98aac3b0c9aa7c10c1f
pedro dias <pedrocorchogdev@gmail.com>
2025-05-08 01:18:27 -0300

    feat: ensure toast is shown after task update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
+import { Toaster } from 'sonner'
 import Sidebar from './Sidebar'
 import Tasks from './Tasks'
 
 function App() {
   return (
     <div className="flex">
+      <Toaster toastOptions={{
+        style: {
+          color: '#35383E'
+        }
+      }}/>
       <Sidebar />
       <Tasks />
     </div>
