6abb851af13bb888c5d48ba676f902edd7bc473b
pedro dias <pedrocorchogdev@gmail.com>
2025-05-08 04:12:35 -0300

    feat: ensure task can be added

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -60,6 +60,11 @@ const Tasks = () => {
     toast.success("Tarefa deletada com sucesso!")
   }
 
+  const handleAddTaskSubmit = (task) => {
+    setTasks([...tasks, task])
+    toast.success("Tarefa adicionada com sucesso!")
+  }
+
   return (
     <div className="w-full space-y-6 px-8 py-16">
       <div className="flex w-full justify-between">
@@ -84,6 +89,7 @@ const Tasks = () => {
           <AddTaskDialog
             isOpen={addTaskDialogIsOpen}
             handleClose={() => setAddTaskDialogIsOpen(false)}
+            handleSubmit={handleAddTaskSubmit}
           />
         </div>
       </div>
