0afeff8351597bcd1f9bb2123a29a2f21249ffd2
pedro dias <pedrocorchogdev@gmail.com>
2025-05-08 04:32:43 -0300

    feat: add validation errors to AddTaskDialog

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import InputLabel from "./InputLabel"
 
-const Input = ({ label, ...props }) => {
+const Input = ({ label, errorMessage, ...props }) => {
     return (
       <div className="flex flex-col space-y-1 text-left">
         <InputLabel htmlFor={props.id}>{label}</InputLabel>
@@ -8,6 +8,9 @@ const Input = ({ label, ...props }) => {
           className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
           {...props}
         />
+        {errorMessage && (
+        <p className="text-left text-xs text-red-500">{errorMessage}</p>
+      )}
       </div>
     )
   }
