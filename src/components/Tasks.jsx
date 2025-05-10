278d48302f9afc89384b434c03e8c8712edc3194
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-10 14:05:13 -0300

    feat: add dashboard cards

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,24 +1,15 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
 import { toast } from 'sonner'
 
-import {
-  AddIcon,
-  CloudSunIcon,
-  MoonIcon,
-  SunIcon,
-  TrashIcon,
-} from '../assets/icons'
+import { CloudSunIcon, MoonIcon, SunIcon } from '../assets/icons'
 import { useGetTasks } from '../hooks/data/use-get-tasks'
 import { taskQueryKeys } from '../keys/index'
-import AddTaskDialog from './AddTaskDialog'
-import Button from './Button'
+import Header from './Header'
 import TaskItem from './TaskItem'
 import TasksSeparator from './TasksSeparator'
 
 const Tasks = () => {
   const queryClient = useQueryClient()
-  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
   const { data: tasks } = useGetTasks()
 
   const morningTasks = tasks?.filter((task) => task.time === 'morning')
@@ -49,31 +40,7 @@ const Tasks = () => {
 
   return (
     <div className="w-full space-y-6 px-8 py-16">
-      <div className="flex w-full justify-between">
-        <div>
-          <span className="text-xs font-semibold text-brand-primary">
-            Minhas Tarefas
-          </span>
-          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
-        </div>
-
-        <div className="flex items-center gap-3">
-          <Button color="ghost">
-            Limpar tarefas
-            <TrashIcon />
-          </Button>
-
-          <Button onClick={() => setAddTaskDialogIsOpen(true)}>
-            <AddIcon />
-            Nova tarefa
-          </Button>
-
-          <AddTaskDialog
-            isOpen={addTaskDialogIsOpen}
-            handleClose={() => setAddTaskDialogIsOpen(false)}
-          />
-        </div>
-      </div>
+      <Header subtitle="Minhas Tarefas" title="Minhas Tarefas" />
 
       <div className="rounded-xl bg-white p-6">
         <div className="space-y-3">
